a254d6e852d215a16d6f4e0e6d9ffd3058ff6661
EvaBien <eva.benharira@gmail.com>
2018-12-16 16:05:45 +0100

    a suggestion for getting the test result

--- main.js
+++ main.js
@@ -25,3 +25,12 @@ const doshow = () => {
 inputname.addEventListener('keyup', changename); 
 inputname.addEventListener('focus',focuson);
 input.addEventListener('click',doshow);
+
+//Trying to get the test results
+//var radios = document.getElementsByName('answer');
+//var valeur;
+//for(var i = 0; i < boutons.length; i++){
+// if(boutons[i].checked){
+// valeur = boutons[i].value;
+// }
+//}
