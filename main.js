7b465023a4ea1168d6572232b51fada30562edf7
EvaBien <eva.benharira@gmail.com>
2018-12-16 23:14:41 +0100

    add )

--- main.js
+++ main.js
@@ -11,16 +11,16 @@
 
 var madiv = document.QuerySelector("#answerChoices1")
 
-if (document.getElementsById("#f-option".checked){
+if (document.getElementsById("#f-option".checked)){
     answer1=1;
-} else if (document.getElementsById("#s-option".checked){
+} else if (document.getElementsById("#s-option".checked)){
     answer1=2;
 } else {
     answer1=3;
 }
 
-console.log(document.getElementsById("#f-option".checked);
-console.log(document.getElementsById("#s-option".checked);
-console.log(document.getElementsById("#t-option".checked);
+console.log(document.getElementsById("#f-option".checked));
+console.log(document.getElementsById("#s-option".checked));
+console.log(document.getElementsById("#t-option".checked));
 
 console.log(answer1);
