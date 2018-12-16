a28c6720d233c42ee295480de0ac50e022c511c0
EvaBien <eva.benharira@gmail.com>
2018-12-16 23:18:21 +0100

    ElementS ==> element

--- main.js
+++ main.js
@@ -11,16 +11,16 @@
 
 var madiv = document.querySelector("#answerChoices1")
 
-if (document.getElementsById("#f-option".checked)){
+if (document.getElementById("#f-option".checked)){
     answer1=1;
-} else if (document.getElementsById("#s-option".checked)){
+} else if (document.getElementById("#s-option".checked)){
     answer1=2;
 } else {
     answer1=3;
 }
 
-console.log(document.getElementsById("#f-option".checked));
-console.log(document.getElementsById("#s-option".checked));
-console.log(document.getElementsById("#t-option".checked));
+console.log(document.getElementById("#f-option".checked));
+console.log(document.getElementById("#s-option".checked));
+console.log(document.getElementById("#t-option".checked));
 
 console.log(answer1);
