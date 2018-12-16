ac1f27a90ca2694c2ea69f30bef966de45751521
EvaBien <eva.benharira@gmail.com>
2018-12-16 23:12:18 +0100

    JS test to get the result and print it when you inspect the page

--- main.js
+++ main.js
@@ -9,5 +9,18 @@
 // }
 //}
 
-var result1 = document.getElementsByName("answer").checked;
-console.log(result1);
+var madiv = document.QuerySelector("#answerChoices1")
+
+if (document.getElementsById("#f-option".checked){
+    answer1=1;
+} else if (document.getElementsById("#s-option".checked){
+    answer1=2;
+} else {
+    answer1=3;
+}
+
+console.log(document.getElementsById("#f-option".checked);
+console.log(document.getElementsById("#s-option".checked);
+console.log(document.getElementsById("#t-option".checked);
+
+console.log(answer1);
