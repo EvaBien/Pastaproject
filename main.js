51a47c5b2efc18de8fcba4c3100cc9793c355a6d
EvaBien <eva.benharira@gmail.com>
2018-12-17 16:40:50 +0100

    querySelctor instead of getElementByID

--- main.js
+++ main.js
@@ -11,16 +11,16 @@
 
 var madiv = document.querySelector("#answerChoices1")
 
-if (document.getElementById("#f-option".checked)){
+if (document.querySelector("#f-option".checked)){
     answer1=1;
-} else if (document.getElementById("#s-option".checked)){
+} else if (document.querySelector("#s-option".checked)){
     answer1=2;
 } else {
     answer1=3;
 }
 
-console.log(document.getElementById("#f-option").checked);
-console.log(document.getElementById("#s-option").checked);
-console.log(document.getElementById("#t-option").checked);
+console.log(document.querySelector("#f-option").checked);
+console.log(document.querySelector("#s-option").checked);
+console.log(document.querySelector("#t-option").checked);
 
 console.log(answer1);
