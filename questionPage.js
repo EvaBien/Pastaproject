aec5769f9158ea6e9f17ed47b9858d57f0f5dc0c
EvaBien <eva.benharira@gmail.com>
2018-12-17 23:50:17 +0100

    Add new button type to be able to change what's shown on the page

--- questionPage.js
+++ questionPage.js
@@ -1,1 +1,67 @@
+/global var
+var madiv = document.querySelector("#answerChoices1")
+var answer1;
+var answer2;
+var answer3;
+
+//functions
+function VerifAnswer1(){
+    if (document.querySelector("#f-option").checked){
+        answer1=1;
+    } else if (document.querySelector("#s-option").checked){
+        answer1=2;
+    } else {
+        answer1=3;
+    }
+
+//test part
+console.log(document.querySelector("#f-option").checked);
+console.log(document.querySelector("#s-option").checked);
+console.log(document.querySelector("#t-option").checked);
+
+console.log(answer1);
+
+}
+
+function VerifAnswer2(){
+    if (document.querySelector("#f-option").checked){
+        answer2=1;
+    } else if (document.querySelector("#s-option").checked){
+        answer2=2;
+    } else {
+        answer2=3;
+    }
+}
+
+function VerifAnswer3(){
+    if (document.querySelector("#f-option").checked){
+        answer3=1;
+    } else if (document.querySelector("#s-option").checked){
+        answer2=2;
+    } else {
+        answer2=3;
+    }
+}
+
+//call
+
+console.log(document.querySelector("#nextButton1"));
+document.querySelector('#nextButton1').addEventListener("click", function(event) {
+  VerifAnswer1();
+  document.querySelector(".set1").style.display="none";
+  document.querySelector(".set2").style.display="inline";
+});
+
+document.querySelector("nextButton2").addEventListener("click", function(event) {
+  VerifAnswer2();
+  window.location = "questions3.html";
+});
+
+document.querySelector("#resultButton").addEventListener("click", function(event) {
+  VerifAnswer3();
+  window.location = "personalityPage.html";
+});
+
+
+
 
