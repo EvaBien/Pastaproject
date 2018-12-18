7ff16234262f487e81a2ec643c86b1e90fe611c1
EvaBien <eva.benharira@gmail.com>
2018-12-19 00:01:45 +0100

    link personnality page with our new questionPgae.js

--- questionPage.js
+++ questionPage.js
@@ -62,3 +62,29 @@ document.querySelector("#resultButton").addEventListener("click", function(event
   window.location = "personalityPage.html";
 });
 
+
+//show results
+
+// if the 3 answers are same
+if ( (answer1==answer2) & (answer1==answer3) ) {
+    if (answer1==1) { document.getElementByClassName("elegant").style.display = "inline"; }
+    else if (answer1==2) { document.getElementByClassName("crazy").style.display = "inline"; }
+    else { document.getElementByClassName("shy").style.display = "inline"; }
+}
+
+// if the 3 answers are different
+if ( (answer1!=answer2) & (answer1!=answer3) & (answer2!=answer3) ) { document.getElementByClassName("creapyPasta").style.display = "inline"; }
+
+// if there are different proportions
+if ( (answer1+answer2+answer3==4) ) { document.getElementByClassName("elegant").style.display = "inline"; }
+if ( (answer1+answer2+answer3==8) ) { document.getElementByClassName("shy").style.display = "inline"; }
+if ( (answer1+answer2+answer3==5) ) {
+    if ( (answer1==3) | (answer2==3) | (answer3==3) ) { document.getElementByClassName("elegant").style.display = "inline"; }
+    else { document.getElementByClassName("crazy").style.display = "inline"; }
+}
+if ( (answer1+answer2+answer3==7) ) {
+    if ( (answer1==2) | (answer2==2) | (answer3==2) ) { document.getElementByClassName("crazy").style.display = "inline"; }
+    else { document.getElementByClassName("shy").style.display = "inline"; }
+}
+
+
